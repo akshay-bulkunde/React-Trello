e4d77d14ed89c568a5a305e4d3eeb94f6bd3a363
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-11-03 12:36:07 +0530

    Added error boundary with get cards inside list and create checklist inside checkitems

---
+++ src/components/CheckList/CheckList.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import CustomButton from "../CustomButton/CustomButton";
+import axios from "axios";
+import CustomTextField from "../CustomTextField/CustomTextField";
+import CheckItem from "../CheckItem/CheckItem";
+import { apiKey, token, baseURL } from "../../apiConfig/apiConfig";
+import { useSnackbar } from "notistack";
+
+const CheckList = ({ checkList, deleteCheckList }) => {
+  let [checkItems, setCheckItems] = useState(checkList.checkItems);
+  let [addCheckItemView, setAddCheckItemView] = useState(false);
+  let [checkItemInput, setCheckItemInput] = useState("");
+  const { enqueueSnackbar } = useSnackbar();
+
+  let createCheckItem = async (checkListId) => {
+    if (checkItemInput) {
+      try {
+        let res = await axios.post(
+          `${baseURL}/checklists/${checkListId}/checkItems?name=${checkItemInput}&key=${apiKey}&token=${token}`
+        );
+        setCheckItems([...checkItems, res.data]);
+      } catch (error) {
+        console.log("error during createCheckItem", error);
+        enqueueSnackbar(`Failed to add check item: ${error.message}`, {
+          variant: "error",
+        });
+      } finally {
+        setAddCheckItemView(false);
+      }
+    }
+  };
+
+  let updateCheckItem = async (checkItemId) => {
+    let newState = null;
+    let updatedCheckItems = checkItems.map((item) => {
+      if (item.id == checkItemId) {
+        if (item.id == checkItemId && item.state == "complete") {
+          newState = "incomplete";
+        } else if (item.id == checkItemId && item.state == "incomplete") {
+          newState = "complete";
+        }
+        return { ...item, state: newState };
+      } else {
+        return item;
+      }
+    });
+
+    try {
+      let res = await axios.put(
+        `${baseURL}/cards/${checkList.idCard}/checkItem/${checkItemId}?state=${newState}&key=${apiKey}&token=${token}`
+      );
+      if (res.status == 200) {
+        setCheckItems(updatedCheckItems);
+      }
+    } catch (error) {
+      console.error("Error during updateCheckItem", error);
+      enqueueSnackbar(`Failed to update check item: ${error.message}`, {
+        variant: "error",
+      });
+    }
+  };
+
+  let deleteCheckItem = async (checkItemId) => {
+    try {
+      let res = await axios.delete(
+        `${baseURL}/cards/${checkList.idCard}/checkItem/${checkItemId}?key=${apiKey}&token=${token}`
+      );
+      if (res.status == 200) {
+        setCheckItems(checkItems.filter((items) => items.id != checkItemId));
+      }
+    } catch (error) {
+      console.error("Error during deleteCheckItem", error);
+      enqueueSnackbar(`Failed to delete check item: ${error.message}`, {
+        variant: "error",
+      });
+    }
+  };
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h6" color="#cfd3d7" gutterBottom>
+          {checkList.name}
+        </Typography>
+
+        <CustomButton
+          onClick={() => deleteCheckList(checkList.id)}
+          label={"Delete"}
+        />
+      </Box>
+      <Box sx={{ ml: 2 }} display="flex" flexDirection="column">
+        {checkItems.map((item) => (
+          <CheckItem
+            key={item.id}
+            item={item}
+            updateCheckItem={updateCheckItem}
+            deleteCheckItem={deleteCheckItem}
+          />
+        ))}
+        {addCheckItemView ? (
+          <Box display="flex" alignItems="center" gap={1}>
+            <CustomTextField
+              onChange={(e) => setCheckItemInput(e.target.value)}
+            />
+            <CustomButton
+              onClick={() => createCheckItem(checkList.id)}
+              label={"Add"}
+            />
+            <CustomButton
+              onClick={() => setAddCheckItemView(false)}
+              label={"Cancel"}
+            />
+          </Box>
+        ) : (
+          <Box>
+            <CustomButton
+              onClick={() => setAddCheckItemView(true)}
+              label={"Add"}
+            />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default CheckList;
---
+++ src/components/CustomDialogForCheckLists/CustomDialogForCheckLists.jsx
@@ -0,0 +1,154 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import axios from "axios";
+import CustomButton from "../CustomButton/CustomButton";
+import CheckList from "../CheckList/CheckList";
+import CustomTextField from "../CustomTextField/CustomTextField";
+import { apiKey, token, baseURL } from "../../apiConfig/apiConfig";
+import { useSnackbar } from "notistack";
+
+const CustomDialog = ({ openModal, setOpenModal, cardId, cardName }) => {
+  const [checkLists, setCheckLists] = useState([]);
+  const [createCheckListView, setCreateCheckListView] = useState(false);
+  const [checkListNameInput, setCheckListNameInput] = useState("");
+  const [loader, setLoader] = useState(true);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const getCheckLists = async () => {
+    if (cardId) {
+      try {
+        const res = await axios.get(
+          `${baseURL}/cards/${cardId}/checklists?key=${apiKey}&token=${token}`
+        );
+        setCheckLists(res.data);
+      } catch (error) {
+        console.error("Error getting checklist", error);
+        enqueueSnackbar(`Failed to get checklists: ${error.message}`, {
+          variant: "error",
+        });
+      } finally {
+        setLoader(false);
+      }
+    }
+  };
+
+  const createCheckList = async () => {
+    if (checkListNameInput) {
+      try {
+        const res = await axios.post(
+          `${baseURL}/cards/${cardId}/checklists?name=${checkListNameInput}&key=${apiKey}&token=${token}`
+        );
+        console.log(res.data);
+        setCheckLists([...checkLists, res.data]);
+      } catch (error) {
+        console.error("Error creating checklist", error);
+        enqueueSnackbar(`Failed to create checklist: ${error.message}`, {
+          variant: "error",
+        });
+      } finally {
+        setCreateCheckListView(false);
+      }
+    }
+  };
+
+  const deleteCheckList = async (checkListId) => {
+    try {
+      const res = await axios.delete(
+        `${baseURL}/checklists/${checkListId}?key=${apiKey}&token=${token}`
+      );
+      if (res.status == 200) {
+        setCheckLists(
+          checkLists.filter((checkList) => checkList.id != checkListId)
+        );
+      }
+    } catch (error) {
+      console.error("Error deleting checklist", error);
+      enqueueSnackbar(`Failed to delete checklist: ${error.message}`, {
+        variant: "error",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getCheckLists();
+  }, []);
+
+  return (
+    <Dialog
+      open={openModal}
+      onClose={() => setOpenModal(false)}
+      aria-labelledby="dialog-title"
+      maxWidth="md"
+      PaperProps={{
+        sx: {
+          width: "40%",
+          bgcolor: "#363e45",
+          p: 2,
+        },
+      }}
+    >
+      <DialogTitle color="#cfd3d7" id="dialog-title">
+        {cardName}
+      </DialogTitle>
+      {createCheckListView ? (
+        <Box display="flex" alignItems="center" gap={1}>
+          <CustomTextField
+            onChange={(e) => setCheckListNameInput(e.target.value)}
+          />
+
+          <CustomButton onClick={() => createCheckList()} label={"Add"} />
+          <CustomButton
+            onClick={() => setCreateCheckListView(false)}
+            label={"Cancel"}
+          />
+        </Box>
+      ) : (
+        <Box>
+          <CustomButton
+            onClick={() => setCreateCheckListView(true)}
+            label={"Create CheckList"}
+          />
+        </Box>
+      )}
+      <DialogContent dividers sx={{ marginY: 3 }}>
+        {loader ? (
+          <Box display="flex" justifyContent={"center"}>
+            <CircularProgress></CircularProgress>
+          </Box>
+        ) : checkLists.length > 0 ? (
+          checkLists.map((checkList) => (
+            <CheckList
+              checkList={checkList}
+              deleteCheckList={deleteCheckList}
+              key={checkList.id}
+            />
+          ))
+        ) : (
+          <Typography variant="body1" color="#cfd3d7">
+            No checklists available.
+          </Typography>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => setOpenModal(false)}
+          color="primary"
+          variant="contained"
+        >
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default CustomDialog;
